801fca0bb79942ed4b8982d41e1fb564631d1bbb
MikkelTMS <mtamsgaard@gmail.com>
2025-04-23 10:39:03 +0200

    Vi Færdige

--- App/ts-modal-component/src/App.tsx
+++ App/ts-modal-component/src/App.tsx
@@ -1,10 +1,25 @@
-import './App.css'
-function App() {
+import { useState } from "react";
+import { Modal } from "./modal/modal.tsx";
+
+const App = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
   return (
     <>
-     
+      <button className="openButton" onClick={() => setIsModalOpen(true)}>Åbn modal</button>
+      <Modal
+        isOpen={isModalOpen}
+        setIsOpen={setIsModalOpen}
+        backgroundColor="#fffbe6"
+        fontColor="#333"
+        width="600px"
+        height="400px"
+      >
+        <h2>Velkommen!</h2>
+        <p>Dette er Team Træls</p>
+      </Modal>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
